9fd72ea9f51c82673f5264ca486fd1b4471c205f
Zachary Whaley <Zachary_Whaley@trendmicro.com>
2018-07-31 16:45:37 -0500

    Working on better error handling
    Cause I'm trying to learn Rust, not just write some of shit code.

--- src/page.rs
+++ src/page.rs
@@ -6,26 +6,42 @@ use std::fs::File;
 use std::io::Read;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::process::exit;
 
 fn main() {
-    let filename = env::args().nth(1).expect("Missing file arg");
+    let filename = match env::args().nth(1) {
+        Some(f) => f,
+        None => {
+            println!("Missing file arg");
+            exit(1);
+        }
+    }
 
-    let mut save: libc::termios = unsafe { mem::zeroed() };
-    unsafe { libc::tcgetattr(libc::STDIN_FILENO, &mut save); }
-    let mut term = libc::termios {..save};
-    term.c_lflag &= !(libc::ICANON | libc::ECHO);
-    unsafe { libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &term); }
+    // C library calls
+    unsafe {
+        let mut save: libc::termios = mem::zeroed();
+        libc::tcgetattr(libc::STDIN_FILENO, &mut save);
 
-    let winsz: libc::winsize = unsafe { mem::zeroed() };
-    unsafe { libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &winsz); }
+        let mut term = libc::termios {..save};
+        term.c_lflag &= !(libc::ICANON | libc::ECHO);
+        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &term);
+
+        let winsz: libc::winsize = mem::zeroed();
+        libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &winsz);
+    }
     let pagesize = winsz.ws_row;
 
     let file = File::open(filename).expect("Unable to open file");
+
     let mut lines = BufReader::new(file).lines().peekable();
     while let Some(_) = lines.peek() {
         for _ in 0..pagesize {
             match lines.next() {
                 Some(line) => {
+                    match line {
+                        Ok(l) => println!("{}", l),
+                        Err(e) => exit(1);
+                    }
                     println!("{}", line.unwrap());
                 },
                 None => {
