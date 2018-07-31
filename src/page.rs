cf4d6e450360e15d6cdbf62e1ea3314ca3683ea7
Zachary Whaley <Zachary_Whaley@trendmicro.com>
2018-07-31 16:25:04 -0500

    Got something working

--- src/page.rs
+++ src/page.rs
@@ -1,25 +1,40 @@
 extern crate libc;
 
 use std::env;
-use std::fs;
 use std::mem;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+use std::io::BufRead;
+use std::io::BufReader;
 
 fn main() {
-    let filename = env::args().nth(1).expect("Missing file");
-    let contents = fs::read_to_string(filename).expect("File read error");
+    let filename = env::args().nth(1).expect("Missing file arg");
 
     let mut save: libc::termios = unsafe { mem::zeroed() };
-    unsafe {
-        libc::tcgetattr(libc::STDIN_FILENO, &mut save);
-    }
-
+    unsafe { libc::tcgetattr(libc::STDIN_FILENO, &mut save); }
     let mut term = libc::termios {..save};
     term.c_lflag &= !libc::ECHO;
-    unsafe {
-        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &term);
+    unsafe { libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &term); }
+
+    let winsz: libc::winsize = unsafe { mem::zeroed() };
+    unsafe { libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &winsz); }
+    let pagesize = winsz.ws_row;
+
+    let file = File::open(filename).expect("Unable to open file");
+    let mut lines = BufReader::new(file).lines().peekable();
+    while let Some(_) = lines.peek() {
+        for _ in 0..pagesize {
+            match lines.next() {
+                Some(line) => {
+                    println!("{}", line.unwrap());
+                },
+                None => {
+                    break;
+                }
+            }
+        }
     }
 
-    unsafe {
-        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &save);
-    }
+    unsafe { libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &save); }
 }
