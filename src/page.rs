ec39a0f4173bdc8bda8175c6e8451311389dadd8
Zachary Whaley <Zachary_Whaley@trendmicro.com>
2018-08-01 14:49:52 -0500

    Rewrite everything to be more rusty!

--- src/page.rs
+++ src/page.rs
@@ -3,23 +3,47 @@ extern crate libc;
 use std::env;
 use std::mem;
 use std::fs::File;
-use std::io::Read;
-use std::io::BufRead;
 use std::io::BufReader;
+use std::io::prelude::*;
 use std::process::exit;
+use std::io::Error;
+
+fn pagefile(filename: &String, pagesize: u16) -> Result<(), Error> {
+    let file = File::open(filename)?;
+
+    let mut buf = BufReader::new(file);
+    let mut line = String::new();
+    let mut eof = false;
+    while !eof {
+        for _ in 0..pagesize {
+            match buf.read_line(&mut line)? {
+                0 => {
+                    eof = true;
+                    break;
+                },
+                _ => println!("{}", line),
+            }
+        }
+        let mut c = [0;1];
+        std::io::stdin().read_exact(&mut c)?;
+    }
+    Ok(())
+}
 
 fn main() {
     let filename = match env::args().nth(1) {
-        Some(f) => f,
+        Some(filename) => filename,
         None => {
-            println!("Missing file arg");
+            eprintln!("Missing file arg");
             exit(1);
-        }
-    }
+        },
+    };
 
     // C library calls
+    let pagesize: u16;
+    let mut save: libc::termios;
     unsafe {
-        let mut save: libc::termios = mem::zeroed();
+        save = mem::zeroed();
         libc::tcgetattr(libc::STDIN_FILENO, &mut save);
 
         let mut term = libc::termios {..save};
@@ -28,30 +52,20 @@ fn main() {
 
         let winsz: libc::winsize = mem::zeroed();
         libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &winsz);
-    }
-    let pagesize = winsz.ws_row;
-
-    let file = File::open(filename).expect("Unable to open file");
-
-    let mut lines = BufReader::new(file).lines().peekable();
-    while let Some(_) = lines.peek() {
-        for _ in 0..pagesize {
-            match lines.next() {
-                Some(line) => {
-                    match line {
-                        Ok(l) => println!("{}", l),
-                        Err(e) => exit(1);
-                    }
-                    println!("{}", line.unwrap());
-                },
-                None => {
-                    break;
-                }
-            }
-        }
-        let mut c = [0;1];
-        std::io::stdin().read_exact(&mut c).unwrap();
+        pagesize = winsz.ws_row;
     }
 
-    unsafe { libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &save); }
+    let rc = match pagefile(&filename, pagesize) {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            1
+        },
+    };
+
+    unsafe {
+        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &save);
+    }
+
+    exit(rc);
 }
