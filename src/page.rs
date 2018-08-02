35b243913660ba2dc0cc03b61962f90d2e839c1a
Zach Whaley <zachbwhaley@gmail.com>
2018-08-02 07:19:17 -0500

    Reorder imports

--- src/page.rs
+++ src/page.rs
@@ -3,8 +3,8 @@ extern crate libc;
 use std::env;
 use std::fs::File;
 use std::io;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::mem;
 use std::process::exit;
 
