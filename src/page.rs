4c73b7e86bf5d0fa268fd356a25a0c3fe4a75b48
Zachary Whaley <Zachary_Whaley@trendmicro.com>
2018-08-03 16:16:13 -0500

    save my work

--- src/page.rs
+++ src/page.rs
@@ -1,21 +1,24 @@
 extern crate libc;
+extern crate termios;
+#[macro_use(defer)]
+extern crate scopeguard;
 
-use std::env;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::mem;
-use std::process::exit;
 
-fn pagefile(filename: &String, pagesize: u16) -> io::Result<()> {
+use termios::*;
+
+fn pagefile(filename: &str, pagesize: u16) -> io::Result<()> {
     let file = File::open(filename)?;
 
     let mut buf = BufReader::new(file);
-    let mut line = String::new();
     let mut eof = false;
     while !eof {
         for _ in 0..pagesize {
+            let mut line = String::new();
             match buf.read_line(&mut line)? {
                 0 => {
                     eof = true;
@@ -30,42 +33,33 @@ fn pagefile(filename: &String, pagesize: u16) -> io::Result<()> {
     Ok(())
 }
 
-fn main() {
-    let filename = match env::args().nth(1) {
-        Some(filename) => filename,
-        None => {
-            eprintln!("Missing file arg");
-            exit(1);
-        },
-    };
+fn main() -> io::Result<()> {
+    let mut args = std::env::args().skip(1);
+    let filename = args.next().expect("Missing file arg");
 
     // C library calls
     let pagesize: u16;
     let mut save: libc::termios;
+
+    let mut term = Termios::from_fd(libc::STDIN_FILENO)?;
+    tcgetattr(&term);
+    let mut save = Termios {..term};
+    term.c_lflag &= !(ICANON | ECHO);
+    tcset
+
     unsafe {
         save = mem::zeroed();
-        libc::tcgetattr(libc::STDIN_FILENO, &mut save);
+        libc::tcgetattr(, &mut save);
 
-        let mut term = libc::termios {..save};
-        term.c_lflag &= !(libc::ICANON | libc::ECHO);
         libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &term);
 
         let winsz: libc::winsize = mem::zeroed();
         libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &winsz);
         pagesize = winsz.ws_row;
     }
+    defer!(unsafe { libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &save); });
 
-    let rc = match pagefile(&filename, pagesize) {
-        Ok(_) => 0,
-        Err(err) => {
-            eprintln!("Error: {}", err);
-            1
-        },
-    };
+    pagefile(&filename, pagesize)?;
 
-    unsafe {
-        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &save);
-    }
-
-    exit(rc);
+    Ok(())
 }
