5b479f3706dbb6899275ba4e54674da8cb103a4d
Zach Whaley <zachbwhaley@gmail.com>
2018-08-03 07:46:09 -0500

    I'm confused...

--- src/find.rs
+++ src/find.rs
@@ -1,25 +1,25 @@
 use std::env;
-use std::io::Result;
+use std::io;
 use std::collections::VecDeque;
-use std::path::PathBuf;
 use std::ffi::OsString;
+use std::path::PathBuf;
 use std::process::exit;
 
-fn find(query: &String, start: &OsString) -> Result<Vec<PathBuf>> {
-    let first = PathBuf::from(start);
-    let mut dirs = VecDeque::from(vec![first]);
+fn find(start: &OsString, query: &String) -> io::Result<Vec<PathBuf>> {
+    let start = PathBuf::from(start);
+    let mut dirs = VecDeque::from(vec![start]);
     let mut result = Vec::new();
 
     while let Some(dir) = dirs.pop_front() {
         for entry in dir.read_dir()? {
             let path = entry?.path();
+            if path.is_dir() {
+                dirs.push_back(path.clone());
+            }
             if let Some(name) = path.file_name() {
                 if query.is_empty() || query.as_str() == name {
                     result.push(path.clone());
                 }
-            }
-            if path.is_dir() {
-                dirs.push_back(path);
             }
         }
     }
@@ -27,16 +27,16 @@ fn find(query: &String, start: &OsString) -> Result<Vec<PathBuf>> {
 }
 
 fn main() {
-    let query = match env::args().nth(1) {
-        Some(query) => query,
-        None => String::new(),
-    };
-    let start = match env::args().nth(2) {
+    let start = match env::args().nth(1) {
         Some(start) => OsString::from(start),
         None => OsString::from("."),
     };
+    let query = match env::args().nth(2) {
+        Some(query) => query,
+        None => String::new(),
+    };
 
-    match find(&query, &start) {
+    match find(&start, &query) {
         Ok(paths) => {
             for path in paths {
                 if let Some(p) = path.to_str() {
