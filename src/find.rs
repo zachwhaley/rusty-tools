ce4a15269319fd33848be494c8a03909b6bbc87e
Zachary Whaley <Zachary_Whaley@trendmicro.com>
2018-08-14 16:45:22 -0500

    Trying to create an iterator for Find

--- src/find.rs
+++ src/find.rs
@@ -1,7 +1,45 @@
 use std::collections::VecDeque;
 use std::ffi::{OsStr, OsString};
+use std::fs::{self, ReadDir, DirEntry};
 use std::io;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+struct Find<'a> {
+    query: &'a str,
+    start: &'a OsStr,
+    dirs: VecDeque<PathBuf>,
+    entries: ReadDir,
+}
+impl<'a> Find<'a> {
+    fn new(query: &'a str, start: &'a OsStr) -> io::Result<Find<'a>> {
+        let f = Find {
+            query: query,
+            start: start,
+            dirs: VecDeque::new(),
+            entries: fs::read_dir(Path::new(start))?,
+        };
+        Ok(f)
+    }
+}
+impl<'a> Iterator for Find<'a> {
+    type Item = io::Result<DirEntry>;
+    fn next(&mut self) -> Option<io::Result<DirEntry>> {
+        let it = entries.filter(|res| {
+            match res {
+                Ok(entry) => {
+                    let path = entry.path();
+                    if let Some(name) = path.file_name() {
+                        if path.is_dir() {
+                            self.dirs.push_back(path);
+                        }
+                        return self.query == name;
+                    }
+                },
+                Err(err) => false
+            }
+        });
+    }
+}
 
 fn find(query: &str, start: &OsStr) -> io::Result<Vec<PathBuf>> {
     let start = PathBuf::from(start);
