a60f6ff131b769e7134e84bc3b1a73bc9ff6880b
Zachary Whaley <Zachary_Whaley@trendmicro.com>
2018-09-04 17:01:45 -0500

    iterator stuff

--- src/find.rs
+++ src/find.rs
@@ -5,71 +5,55 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 struct Find<'a> {
-    query: &'a str,
+    query: &'a OsStr,
     start: &'a OsStr,
+    readdir: io::Result<ReadDir>,
     dirs: VecDeque<PathBuf>,
-    entries: ReadDir,
 }
 impl<'a> Find<'a> {
-    fn new(query: &'a str, start: &'a OsStr) -> io::Result<Find<'a>> {
-        let f = Find {
+    fn new(query: &'a OsStr, start: &'a OsStr) -> Find<'a> {
+        Find {
             query: query,
             start: start,
+            readdir: fs::read_dir(Path::new(start)),
             dirs: VecDeque::new(),
-            entries: fs::read_dir(Path::new(start))?,
-        };
-        Ok(f)
+        }
     }
 }
 impl<'a> Iterator for Find<'a> {
-    type Item = io::Result<DirEntry>;
-    fn next(&mut self) -> Option<io::Result<DirEntry>> {
-        let it = entries.filter(|res| {
-            match res {
-                Ok(entry) => {
-                    let path = entry.path();
-                    if let Some(name) = path.file_name() {
-                        if path.is_dir() {
-                            self.dirs.push_back(path);
-                        }
-                        return self.query == name;
-                    }
-                },
-                Err(err) => false
+    type Item = io::Result<DirEntry>
+    fn next(&mut self) -> Option<Self::Item> {
+        let dirs = &self.dirs;
+        let query = &self.query;
+        let mut readdir = self.readdir.unwrap();
+
+        readdir.find(|entry| {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_dir() {
+                    dirs.push_back(path.clone());
+                }
+                if let Some(name) = path.file_name() {
+                    return *query == name;
+                }
             }
-        });
+            return false;
+        })
     }
 }
 
-fn find(query: &str, start: &OsStr) -> io::Result<Vec<PathBuf>> {
-    let start = PathBuf::from(start);
-    let mut dirs = VecDeque::from(vec![start]);
-    let mut result = Vec::new();
-
-    while let Some(dir) = dirs.pop_front() {
-        for entry in dir.read_dir()? {
-            let path = entry?.path();
-            if path.is_dir() {
-                dirs.push_back(path.clone());
-            }
-            if let Some(name) = path.file_name() {
-                if query.is_empty() || query == name {
-                    result.push(path.clone());
-                }
-            }
-        }
-    }
-    Ok(result)
+fn find<'a>(query: &'a OsStr, start: &'a OsStr) -> Find<'a> {
+    Find::new(query, start)
 }
 
 fn main() -> io::Result<()> {
     let mut args = std::env::args().skip(1);
-    let query = args.next().unwrap_or(String::new());
+    let query = args.next().map(OsString::from).unwrap();
     let start = args.next().map(OsString::from).unwrap_or(OsString::from("."));
 
-    for path in find(&query, &start)? {
-        if let Some(p) = path.to_str() {
-            println!("{}", p);
+    for entry in find(&query, &start) {
+        if let Some(result) = entry?.path().to_str() {
+            println!("{}", result);
         }
     }
     Ok(())
